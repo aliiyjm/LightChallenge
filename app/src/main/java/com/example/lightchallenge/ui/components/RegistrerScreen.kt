f9731bca1ed41b7e6e5a6ebfc2fdea386e7ee686
JNMC0 <20243ds126@utez.edu.mx>
2025-12-09 23:28:09 -0600

    agregue el archivo RegistrerScreen.kt (jorge)

---
+++ app/src/main/java/com/example/lightchallenge/ui/components/RegistrerScreen.kt
@@ -0,0 +1,46 @@
+package com.example.lightchallenge.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.lightchallenge.data.local.UserEntity
+import com.example.lightchallenge.ui.viewmodel.GameViewModel
+
+@Composable
+fun RegisterScreen(viewModel: GameViewModel, onRegisterSuccess: () -> Unit) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Registro de Usuario", style = MaterialTheme.typography.headlineMedium)
+        Spacer(Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuario") }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = {
+            viewModel.registerUser(UserEntity(username = username, password = password))
+            onRegisterSuccess()
+        }) {
+            Text("Registrar")
+        }
+    }
+}
