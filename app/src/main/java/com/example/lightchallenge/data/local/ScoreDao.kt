602c62e3001cd38d4f05ad9848483d483ca42dd4
Mauricio Tadeo Chagoyan Cortes <20243ds137@utez.edu.mx>
2025-12-10 19:17:30 -0600

    Agrege la clase ScoreDao que es el controller

--- app/src/main/java/com/example/lightchallenge/data/local/ScoreDao.kt
+++ app/src/main/java/com/example/lightchallenge/data/local/ScoreDao.kt
@@ -1,2 +1,19 @@
 package com.example.lightchallenge.data.local
 
+import androidx.room.*
+import androidx.room.Dao
+
+@Dao
+interface ScoreDao {
+    @Insert
+    suspend fun insert(score: ScoreEntity)
+
+    @Update
+    suspend fun update(score: ScoreEntity)
+
+    @Delete
+    suspend fun delete(score: ScoreEntity)
+
+    @Query
+    suspend fun getAllScores(): List<ScoreEntity>
+}
